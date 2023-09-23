17090b1995aa5b8b93b338919b4c97cc50eda7b8
Mehmet Genç <gencaslan555@gmail.com>
2023-09-23 22:30:20 +0300

    v2

--- src/Class01.java
+++ src/Class01.java
@@ -2,6 +2,9 @@ public class Class01 {
     public static void main(String[] args) {
         //v1
         System.out.println("versiyon1");
+        //v2
+        System.out.println("versiyon 2");
+
 
 
 
